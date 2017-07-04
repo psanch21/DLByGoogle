e52083d11d9e644aa0e2ee133fd0328152db39f0
Pablo <psanch2103@gmail.com>
2017-07-04 02:37:10 +0200

    Update Assignement 2

---
+++ aux.py
@@ -0,0 +1,10 @@
+def reformat(dataset, labels):
+  dataset = dataset.reshape((-1, image_size * image_size)).astype(np.float32)
+  # Map 0 to [1.0, 0.0, 0.0 ...], 1 to [0.0, 1.0, 0.0 ...]
+  labels = (np.arange(num_labels) == labels[:,None]).astype(np.float32)
+  return dataset, labels
+
+def accuracy(predictions, labels):
+    return (100.0 * np.sum(np.argmax(predictions, 1) == np.argmax(labels, 1))
+          / predictions.shape[0])
+
